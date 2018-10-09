4d37aade4687c6c3a9f003b128d7435c8148db5c
Mary Jack <maryjackinthebox@gmail.com>
2018-10-09 20:06:30 +0000

    automatically backed up by learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,21 @@
 class Ride < ActiveRecord::Base
+
+  def change
+    create_table :taxi do |t|
+      t.string :name
+      t.timestamps
+    end
+ 
+    create_table :passenger do |t|
+      t.string :name
+      t.timestamps
+    end
+ 
+    create_table :ride do |t|
+      t.belongs_to :taxi, index: true
+      t.belongs_to :passenger, index: true
+      t.timestamps
+    end
+  end
 end
+
